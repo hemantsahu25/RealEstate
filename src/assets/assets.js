61368f4386bdc2cacbfb45ada30fb3493c91cdae
hemantsahu25 <hemantsahu8115@gmail.com>
2025-08-13 12:09:29 +0530

    add file

---
+++ src/assets/assets.js
@@ -0,0 +1,67 @@
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+
+export const assets = {    
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Skyline Haven",
+      price: "INR 2500000",
+      location: "New Delhi",
+      image: project_img_1
+    },
+    {
+      title: "Vista Verde",
+      price: "INR 28000000",
+      location: "Mumbai",
+      image: project_img_2
+    },
+    {
+      title: "Serenity Suites",
+      price: "INR 3500000",
+      location: "Mumbai",
+      image: project_img_3
+    },
+    {
+      title: "Central Square",
+      price: "INR 2900000",
+      location: "Banglore",
+      image: project_img_4
+    },
+    {
+      title: "Vista Verde",
+      price: "INR 2500000",
+      location: "Mumbai",
+      image: project_img_5
+    },
+    {
+      title: "Serenity Suites",
+      price: "INR 15000000",
+      location: "Banglore",
+      image: project_img_6
+    },
+    
+  ];
---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { motion } from "framer-motion";
+
+const About = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 200 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
+      className="flex flex-col items-center justify-center mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
+      id="About"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 pt-10">
+        About{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Our Brand
+        </span>
+      </h1>
+      <p className="text-gray-500 max-w-80 text-center mb-8">
+        Passonate About Properties, Dedicated to Your Vision
+      </p>
+      <div className="flex flex-col md:flex-row items-center md:items-start md:gap-20">
+        {/* --- Left-Side ---*/}
+        <img
+          src={assets.brand_img}
+          className="w-full sm:w-1/2 max-w-lg"
+          alt=""
+        />
+        {/* --- Right-Side ---*/}
+        <div className="flex flex-col items-center md:items-start mt-10 text-gray-600">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl pr-28">
+            <div>
+              <p className="text-4xl font-medium text-gray-800">12+</p>
+              <p>Years of Excellence</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">14+</p>
+              <p>Projects Completed</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">20+</p>
+              <p>Mn. Sq. Ft. Delivered</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">25+</p>
+              <p>Ongoing Projects</p>
+            </div>
+          </div>
+          <p className="my-10 max-w-lg">
+            As a dedicated real estate professional. I strive to make every client journey seamless and rewarding. With a deep understanding of the local market and a commitment to tailored service, I guide you confidently from listing to closing. Whether you're a first-time buyer or seasoned investor, I leverage innovative marketing, strong industry relationships, and open communication to help you achieve your real estate goals. Let’s turn your property dreams into reality—get in touch today."
+          </p>
+          <button className="bg-blue-600 text-white px-8 py-2 rounded hover:bg-blue-900">
+            Learn More
+          </button>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default About;
